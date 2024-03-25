5a8b2f3a0c8451d9b6dbaec70eb2424fc96fe546
FahmiSar <fahmi.sardar@ucalgary.ca>
2024-03-25 15:59:36 -0600

    added a text to the overall total cost

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -26,7 +26,7 @@ const Cart = ({ cart, setCartData}) => {
       {cart.map(item => (
         <CartItem key={item.id} item={item} removeFromCart = {removeFromCart}/>
       ))}
-      <h3>Total Cost: ${calculateTotalCost()}</h3>
+      <h3>Total Cost (in cart): ${calculateTotalCost()}</h3>
     </div>
   );
 };
