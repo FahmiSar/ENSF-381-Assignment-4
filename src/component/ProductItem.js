73824d432c7e0c134566c9b4f605d33b95c45d0d
safaa tazin <144169864+safaatazin@users.noreply.github.com>
2024-03-25 13:46:29 -0600

    Update ProductItem.js

--- src/component/ProductItem.js
+++ src/component/ProductItem.js
@@ -1,25 +1,30 @@
 import React, { useState } from 'react';
 
-const ProductItem = ({ product, addToCart }) => {
+const ProductItem = ({ product, addToCart}) => {
   const [showDetails, setShowDetails] = useState(false);
 
   const toggleDetails = () => {
     setShowDetails(!showDetails);
   };
 
+  const handleClick = () =>{
+    addToCart(product);
+  }
+
   return (
     <div className="product-item" onMouseEnter={toggleDetails} onMouseLeave={toggleDetails}>
       <img src={product.image} alt={product.name} />
       <div className="product-details">
         <p className="product-name">{product.name}</p>
-        <p className="product-price">{product.price}</p>
+        <p className="product-price">${product.price}</p>
+        
         {showDetails && (
           <div className="product-description">
             <p>{product.description}</p>
           </div>
         )}
       </div>
-      <button onClick={() => addToCart(product)}>Add to Cart</button>
+      <button onClick = {handleClick}>Add to Cart</button>
     </div>
   );
 };
